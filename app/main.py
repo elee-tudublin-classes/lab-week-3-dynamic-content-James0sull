d0c2a95b20307e8b15925e12bcba727d05244217
X00202782 James O Sullivan <x00202782@mytudublin.ie>
2024-09-30 11:16:21 +0100

    lab3 commit 1

---
+++ app/main.py
@@ -0,0 +1,24 @@
+# import dependencies
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+# create app instance
+app = FastAPI()
+
+# set location for templates
+templates = Jinja2Templates(directory="app/view_templates")
+
+# handle http get requests for the site root /
+# return the index.html page
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static",
+)
+
